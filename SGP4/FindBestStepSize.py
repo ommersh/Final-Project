30b307ad44bd04e3f2bceac9bc863008490b146b
Omer <ommersh@gmail.com>
2023-06-10 17:12:18 +0300

    Add saving data to file

--- SGP4/FindBestStepSize.py
+++ SGP4/FindBestStepSize.py
@@ -2,36 +2,37 @@ import numpy as np
 import math
 from sgp4 import omm
 from sgp4.api import Satrec
-from SgpMathUtils import GetJdAndFrArrayForSattelite
-
-import SGP4
+from SgpMathUtils import GetJdAndFrArrayForSattelite, savePointToFile
 
 
-def calculate_distance(tle1, tle2, t_end, st_min, p_max, factor, init_step_size):
+def calculate_distance(tle1, tle2, t_end, st_min, p_max, factor, init_step_size, name):
     min_distance = float('inf')
     step_size = init_step_size
     min_time = None
 
-    counter = 2
 
     while True:
         num_point = math.ceil(t_end / step_size)
         num_blocks = math.ceil(num_point / p_max)
         last_min_distance = min_distance
 
+        counter = 2
+
+        sat1 = Satrec()
+        sat2 = Satrec()
+        omm.initialize(sat1, tle1)
+        omm.initialize(sat2, tle2)
+        sat_to_get_time = sat1 if sat1.jdsatepoch > sat2.jdsatepoch else sat2
+
+        print("num_point", num_point)
+
         for block in range(num_blocks):
             start_time = block * p_max * step_size
             end_time = min(t_end, (block + 1) * p_max * step_size)
 
+            print("start_time", start_time, "end_time", end_time)
+
             time_points = np.arange(start_time, end_time + step_size, step_size)
-
-            sat1 = Satrec()
-            sat2 = Satrec()
-
-            omm.initialize(sat1, tle1)
-            omm.initialize(sat2, tle2)
-
-            sat_to_get_time = sat1 if sat1.jdsatepoch > sat2.jdsatepoch else sat2
 
             jd, fr = GetJdAndFrArrayForSattelite(sat_to_get_time, time_points)
 
@@ -47,6 +48,8 @@ def calculate_distance(tle1, tle2, t_end, st_min, p_max, factor, init_step_size)
 
             min_idx = np.argmin(distance_vector)
 
+            savePointToFile(distance_vector, time_points, name + '_' + str(step_size))
+
             if distance_vector[min_idx] < min_distance:
                 min_distance = distance_vector[min_idx]
                 min_time = time_points[min_idx]
@@ -55,31 +58,33 @@ def calculate_distance(tle1, tle2, t_end, st_min, p_max, factor, init_step_size)
                 print("r2: ", r2[min_idx])
 
         step_size /= factor
-        if min_distance - last_min_distance > 0:
+        if min_distance - last_min_distance >= 0:
             counter = counter -1
         if step_size < st_min or counter <= 0:
             print("distance: ", min_distance)
-
             break
 
     return min_distance, min_time
 
 
 t_end = 1209600  # two weeks
-st_min = 0.001  # millisecond
+st_min = 0.01  # millisecond
 
-factor = 10
+factor = 2
 p_max = t_end / st_min
 
-init_step_size = 1
+init_step_size = 0.4
 
-with open('LEMUR2.xml') as f:
+sat1_name = 'LEMUR2'
+sat2_name = 'COSMOS'
+with open(sat1_name + '.xml') as f:
     tle1 = next(omm.parse_xml(f))
 
-with open('COSMOS.xml') as f:
+with open(sat2_name + '.xml') as f:
     tle2 = next(omm.parse_xml(f))
 
-result = calculate_distance(tle1, tle2, t_end, st_min, p_max, factor, init_step_size)
+filename = sat1_name + '_' + sat2_name
+result = calculate_distance(tle1, tle2, t_end, st_min, p_max, factor, init_step_size, filename)
 
 print("The minimal distance is:", result[0], "in time:", result[1])
 
