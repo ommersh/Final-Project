9cf590655b5e7af8174ed1f843a7fb402c830944
HallelWeil <101136525+HallelWeil@users.noreply.github.com>
2023-06-16 18:13:52 +0300

    -Added print format to print the result in a table

--- Implementations/Implementations/Implementations.cpp
+++ Implementations/Implementations/Implementations.cpp
@@ -7,13 +7,17 @@
 #include "ANCAS.h"
 #include <vector>
 #include <chrono>
-
+#include <iomanip>
 
 double getCurrentTymeInMicroSec();
 void runCatch();
 void runAncas();
+void startPrint();
+void printResult(string algName, string testName, int numberOfPoints, double runTime, TCA tca);
+
 int main()
 {
+    startPrint();
     runCatch();
     runAncas();
     return 0;
@@ -32,8 +36,9 @@ void runCatch()
     startTime = getCurrentTymeInMicroSec();
     TCA tca = c.CatchAlgorithm(fileData.data, timePoints, lastPointIndex);
     endTime = getCurrentTymeInMicroSec();
-    std::cout << "Catch result:\nTime: " << tca.time << "\nDistance:" << tca.distance << "\n";
-    std::cout << "Catch took:\n " << endTime - startTime << " micro seconds\n" << (endTime - startTime) / 1000000 << " seconds \n";
+    //std::cout << "Catch result:\nTime: " << tca.time << "\nDistance:" << tca.distance << "\n";
+    //std::cout << "Catch took:\n " << endTime - startTime << " micro seconds\n" << (endTime - startTime) / 1000000 << " seconds \n";
+    printResult("CATCH", "LEMUR2_COSMOS", lastPointIndex + 1, endTime - startTime, tca);
     if (fileData.data != nullptr)
     {
         delete[] fileData.data;
@@ -44,22 +49,54 @@ void runAncas()
     //run ancas
     ANCAS a;
     FileReader fr;
-    sFileData fileData = fr.readDataFromFile("../../SGP4/data/LEMUR2_COSMOS_GAUSS.csv");
+    sFileData fileData = fr.readDataFromFile("../../SGP4/data/LEMUR2_COSMOS_CONST.csv");
     double* timePoints = fileData.timePoints;
     int lastPointIndex = fileData.size;
     double startTime, endTime;
     double temp;
     startTime = getCurrentTymeInMicroSec();
-    TCA tca = a.ANCASAlgorithm(fileData.data , timePoints, lastPointIndex);
+    TCA tca = a.ANCASAlgorithm(fileData.data, timePoints, lastPointIndex);
     endTime = getCurrentTymeInMicroSec();
-    std::cout << "Ancas result:\nTime: " << tca.time << "\nDistance:" << tca.distance << "\n";
-    std::cout << "Ancas took:\n " << endTime - startTime << " micro seconds\n" << (endTime - startTime) / 1000000 << " seconds \n";
+    //std::cout << "Ancas result:\nTime: " << tca.time << "\nDistance:" << tca.distance << "\n";
+    //std::cout << "Ancas took:\n " << endTime - startTime << " micro seconds\n" << (endTime - startTime) / 1000000 << " seconds \n";
+    printResult("ANCAS", "LEMUR2_COSMOS", lastPointIndex + 1, endTime - startTime, tca);
     if (fileData.data != nullptr)
     {
         delete[] fileData.data;
     }
 }
 
+void printResult(string algName, string testName, int numberOfPoints, double runTime, TCA tca)
+{
+    std::cout << std::left << std::setw(15) << testName
+        << "|" << std::left << std::setw(15) << algName
+        << "|" << std::left << std::setw(20) << numberOfPoints
+        << "|" << std::left << std::setw(20) << runTime / 1000000
+        << "|" << std::left << std::setw(20) << runTime
+        << "|" << std::left << std::setw(15) << tca.distance
+        << "|" << std::left << std::setw(15) << tca.time << std::endl;
+
+}
+void startPrint()
+{
+    std::cout << std::left << std::setw(15) << "testName"
+        << "|" << std::left << std::setw(15) << "algName"
+        << "|" << std::left << std::setw(20) << "numberOfPoints"
+        << "|" << std::left << std::setw(20) << "runTime(sec)"
+        << "|" << std::left << std::setw(20) << "runTime(microSec)"
+        << "|" << std::left << std::setw(15) << "TCA distance"
+        << "|" << std::left << std::setw(15) << "TCA time" << std::endl;
+
+
+    std::cout << std::left << std::setw(15) << std::setfill('-') << ""
+        << "|" << std::left << std::setw(15) << ""
+        << "|" << std::left << std::setw(20) << ""
+        << "|" << std::left << std::setw(20) << ""
+        << "|" << std::left << std::setw(20) << ""
+        << "|" << std::left << std::setw(15) << ""
+        << "|" << std::left << std::setw(15) << "" << std::setfill(' ') << std::endl;
+
+}
 double getCurrentTymeInMicroSec()
 {
     // Get the current time point
